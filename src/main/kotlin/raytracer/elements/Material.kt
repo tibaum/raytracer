6fd560fec617cb00407c437d82d14d4a9b73625a
tibaum <tibaum3@gmail.com>
2022-02-26 11:03:35 +0100

    Correct typo

--- src/main/kotlin/raytracer/elements/Material.kt
+++ src/main/kotlin/raytracer/elements/Material.kt
@@ -4,10 +4,10 @@ package raytracer.elements
  * Material encapsulates the surface color and attributes from the Phong reflection model.
  *
  * @param surfaceColor color of the material
- * @param ambientReflection background light or light reflected from other objects in the environment, a nonnegative value, typically between 0 an 1
- * @param diffuseReflection light reflected from a matte surface, a nonnegative value, typically between 0 an 1
- * @param specularReflection reflection of the light source itself, a nonnegative value, typically between 0 an 1
- * @param shininess controls size and tightness of the specular highlight, a nonnegative value, typically between 10 an 200
+ * @param ambientReflection background light or light reflected from other objects in the environment, a nonnegative value, typically between 0 and 1
+ * @param diffuseReflection light reflected from a matte surface, a nonnegative value, typically between 0 and 1
+ * @param specularReflection reflection of the light source itself, a nonnegative value, typically between 0 and 1
+ * @param shininess controls size and tightness of the specular highlight, a nonnegative value, typically between 10 and 200
  */
 data class Material(
     val surfaceColor: Tuple,
